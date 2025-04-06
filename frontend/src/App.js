c78e7257df49d80ad8152b721b8f2536df1d7376
Athena <128306886+athens-igg@users.noreply.github.com>
2025-04-06 21:21:18 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./auth/AuthContext"; 
+import { AuthProvider } from "./auth/AuthContext";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
@@ -12,15 +12,20 @@ import DestinationSearch from "./pages/DestinationSearch";
 import Gallery from "./pages/Gallery";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import SavedItinerary from "./components/SavedItinerary";
 import "./App.css";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="app-container"> {/* New wrapper */}
+        <div className="app-container">
+          {" "}
+          {/* New wrapper */}
           <Navbar />
-          <div className="main-content"> {/* Ensures content expands */}
+          <div className="main-content">
+            {" "}
+            {/* Ensures content expands */}
             <Routes>
               <Route path="/" element={<Homepage />} />
               <Route path="/login" element={<Login />} />
@@ -28,9 +33,13 @@ function App() {
               <Route path="/profile" element={<Profile />} />
               <Route path="/trip-planner" element={<TripPlanner />} />
               <Route path="/itinerary/:id" element={<Itinerary />} />
-              <Route path="/destination-search" element={<DestinationSearch />} />
+              <Route
+                path="/destination-search"
+                element={<DestinationSearch />}
+              />
               <Route path="/feedback" element={<Feedback />} />
               <Route path="/gallery" element={<Gallery />} />
+              <Route path="/saved-itinerary/:id" element={<SavedItinerary />} />
             </Routes>
           </div>
           <Footer />
